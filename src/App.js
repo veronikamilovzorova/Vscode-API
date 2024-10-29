e263b51bbfad3ebc048b1391a181203c55766114
Veronika Milovzorova <veronika.milovzorova@gmail.com>
2024-10-29 15:03:51 +0200

    vscode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pakiautomaadid, setPakiautomaadid] = useState([]);
+
+  useEffect(() => {
+    fetch("https://localhost:7275/api/ParcelMachine")
+      .then(res => res.json())
+      .then(json => setPakiautomaadid(json));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select>
+        {pakiautomaadid.map(automaat => 
+            <option>
+                {automaat.NAME}
+            </option>)}
+      </select>
     </div>
   );
 }
